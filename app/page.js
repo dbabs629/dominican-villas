3289f3ea2eb80e925719b93016970afef0074183
daniel <daniel.l.babin@gmail.com>
2024-01-10 14:08:54 -0500

    added sousa images to home page

--- app/page.js
+++ app/page.js
@@ -3,6 +3,10 @@ import Layout from './components/Layout'
 import HeroSection from './components/HeroSection'
 import MamasParadiseSection from './components/MamasParadiseSection'
 import SunsetOasisSection from './components/SunsetOasisSection'
+import SectionHeading from './components/SectionHeading'
+import Image from 'next/image'
+import sousaimg1 from '../public/images/sousa-img-1.PNG'
+import sousaimg2 from '../public/images/sousa-img-2.PNG'
 
 export default function Home() {
   return (
@@ -15,6 +19,26 @@ export default function Home() {
         <HeroSection />
         <MamasParadiseSection />
         <SunsetOasisSection />
+        <div>
+          <SectionHeading title='Sousa, Dominican Republic' />
+          <div>
+            <Image
+              className='object-cover h-auto max-w-full rounded-xl'
+              src={sousaimg1}
+              width='1875'
+              height='622'
+              alt='something 2'
+            />
+            <Image
+              className='object-cover h-auto max-w-full rounded-xl'
+              src={sousaimg2}
+              width='746'
+              height='596'
+              alt='something 2'
+            />
+            <p></p>
+          </div>
+        </div>
       </main>
     </Layout>
   )
