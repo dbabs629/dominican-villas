16bb74b4f45a103725a6a037f4356b48e9c40c8f
daniel <daniel.l.babin@gmail.com>
2024-01-16 18:34:34 -0500

    added media queries to calendar

--- app/calendar/page.jsx
+++ app/calendar/page.jsx
@@ -23,10 +23,10 @@ export default function page() {
             <p className="animate-pulse"> Loading...</p>
           </div>
         )}
-        <article className="relative mx-auto h-96 w-full">
+        <article className="relative mx-auto mb-20 h-96 w-full xs:h-[400px] sm:h-[500px] lg:h-[700px] xl:h-full">
           <span className="absolute z-0 h-auto w-full animate-pulse rounded-xl bg-gray-300" />
           <iframe
-            className="z-20 max-h-full max-w-full"
+            className="z-20 mx-auto max-h-full max-w-full sm:w-4/5"
             src="https://calendar.google.com/calendar/embed?height=1000&wkst=1&bgcolor=%23ffffff&ctz=America%2FToronto&showTitle=0&showPrint=0&showTabs=0&showCalendars=0&showTz=0&src=YjQxOTMzZWIyYjBkOTAxNTFhZWM1NDAzZTBkMGY1YzBjNWEwMjljMjJiZDFmZTZiNWUxYWUwMTExMjc3MWQwNEBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&color=%6691c6"
             width="1999"
             height="1000"
